6f73dbf75aa6520a350fd4484eea52e319e66b56
dinoxor <quangmai09@gmail.com>
2019-03-09 00:16:31 -0600

    add ef migration

---
+++ CarvedRock.Api/CarvedRock.Api/CarvedRockContext.cs
@@ -0,0 +1,25 @@
+﻿using CarvedRock.Api.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api
+{
+    public class CarvedRockContext : DbContext
+    {
+        public CarvedRockContext(DbContextOptions<CarvedRockContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+}
---
+++ CarvedRock.Api/CarvedRock.Api/Migrations/20190309061456_RenameProductType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarvedRock.Api.Migrations
+{
+    public partial class RenameProductType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductType",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductType",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
