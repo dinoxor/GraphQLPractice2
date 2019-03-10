5437498dca351283ea8362300250bbfc4cfee837
dinoxor <quangmai09@gmail.com>
2019-03-10 17:37:27 -0500

    Add Seed Data

--- CarvedRock.Api/CarvedRock.Api/Migrations/20190309062222_AddSeedData.Designer.cs
+++ CarvedRock.Api/CarvedRock.Api/Migrations/20190309062222_AddSeedData.Designer.cs
@@ -4,14 +4,16 @@ using CarvedRock.Api;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarvedRock.Api.Migrations
 {
     [DbContext(typeof(CarvedRockContext))]
-    partial class CarvedRockContextModelSnapshot : ModelSnapshot
+    [Migration("20190309062222_AddSeedData")]
+    partial class AddSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
